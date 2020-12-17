e1f63f6cf8346101a8adef3d51ca96e4e16fe176
Dmitry Khohlov <70712590+DiametrFQ@users.noreply.github.com>
2020-12-17 19:37:49 +0500

    I am trying to make the code more readable

--- script.js
+++ script.js
@@ -1,86 +1,88 @@
-let score = +localStorage.getItem('score');// Preserve the color of the overall result
-let bestScore = +localStorage.getItem('bestscore');//Keeping the color of the Best Score
-let bgcolor = localStorage.getItem('bgcolor');//Keeping the color of the background
-let snacolor = localStorage.getItem('snacolor');//Keeping the color of the snake
-let eacolor = localStorage.getItem('eacolor');//Keeping the color of the food
+let score = +localStorage.getItem('score')// Preserve the color of the overall result
+let bestScore = +localStorage.getItem('bestscore')//Keeping the color of the Best Score
+let bgcolor = localStorage.getItem('bgcolor')//Keeping the color of the background
+let snacolor = localStorage.getItem('snacolor')//Keeping the color of the snake
+let eacolor = localStorage.getItem('eacolor')//Keeping the color of the food
+
 const navUA = navigator.userAgent;
-const center = document.querySelector("#center");
-window.onload = function(){
-	if (bgcolor==null||snacolor==null||eacolor==null){
-		bgcolor = '#00ff00';
-		snacolor = '#0000ff';
-		eacolor = '#ffb500';
-	}
-	document.querySelector("#color1").value = bgcolor;
-	document.querySelector("#color2").value = snacolor;
-	document.querySelector("#color3").value = eacolor;
+
+const center = document.querySelector("#center")
+const colBg = document.querySelector("#color1")
+const colSnake = document.querySelector("#color2")
+const colEat = document.querySelector("#color3")
+
+window.onload =()=> {
+	bgcolor === null ? colBg.value = bgcolor = '#00ff00' : colBg.value = bgcolor;
+	snacolor === null ? colSnake.value = snacolor = '#0000ff' : colSnake.value = snacolor;
+	eacolor === null ? colEat.value = eacolor = '#ffb500' : colEat.value = eacolor;
+
 	document.querySelector("#c2").style.backgroundColor = bgcolor;
 	document.querySelector(`#yum`).style.backgroundColor = eacolor;
 		
-	for(i=0;i<14;i++)document.querySelector("#tail").innerHTML += `<div class="tails" id="tails${i}"></div>`;
+	for(i=0;i<14;i++)document.querySelector("#tail").innerHTML += `<div class="tails" id="tails${i}"></div>`
 	
 	for(i=0;i<5;i++){
-		document.querySelector(`#tails${i}`).style.left = 0 + "px";
-		document.querySelector(`#tails${i}`).style.top = 0+30*i+"px";
+		document.querySelector(`#tails${i}`).style.left = 0 + "px"
+		document.querySelector(`#tails${i}`).style.top = 30 * i +"px"
 	}for(i=4;i<12;i++){
-        document.querySelector(`#tails${i}`).style.left= 0+31*(i-4)+"px";
-		document.querySelector(`#tails${i}`).style.top = 120+"px";
+        document.querySelector(`#tails${i}`).style.left = 31 * ( i - 4 ) +"px"
+		document.querySelector(`#tails${i}`).style.top = 120 + "px"
 	}
 	for(i=11;i<14;i++){
-		document.querySelector(`#tails${i}`).style.left= 217+"px";
-		document.querySelector(`#tails${i}`).style.top = 120+31*(i-11)+"px";
+		document.querySelector(`#tails${i}`).style.left = 217 + "px"
+		document.querySelector(`#tails${i}`).style.top = 120 + 31 * ( i - 11 ) +"px"
 	}//Creates a non-playable snake for example
-	for(i=0;i<14;i++) document.querySelector(`#tails${i}`).style.backgroundColor = snacolor;
+	for(i=0;i<14;i++) document.querySelector(`#tails${i}`).style.backgroundColor = snacolor
 }//Start
 
-
-document.querySelector("#color1").oninput = function(){
+colBg.oninput = function()  {
 	bgcolor = this.value;
-	document.querySelector("#c2").style.backgroundColor = bgcolor;
-	localStorage.setItem('bgcolor',bgcolor);
+	document.querySelector("#c2").style.backgroundColor = bgcolor
+	localStorage.setItem('bgcolor',bgcolor)
 }
-document.querySelector("#color2").oninput = function(){
-	snacolor = this.value;
-	localStorage.setItem('snacolor',snacolor);
-	for(i=0;i<14;i++)document.querySelector(`#tails${i}`).style.backgroundColor = snacolor;
+colSnake.oninput = function() {
+	snacolor = this.value
+	localStorage.setItem('snacolor',snacolor)
+	for(i=0;i<14;i++)document.querySelector(`#tails${i}`).style.backgroundColor = snacolor
 }
-document.querySelector("#color3").oninput = function(){
+colEat.oninput = function() {
 	eacolor = this.value;
-	document.querySelector(`#yum`).style.backgroundColor = this.value;
-	localStorage.setItem('eacolor',this.value);
+	document.querySelector(`#yum`).style.backgroundColor = this.value
+	localStorage.setItem('eacolor',this.value)
 }
-document.querySelector("#start").onclick = () => Two();
-document.onkeydown = (event)=> {if(event.key === "Enter")Two() }
+document.querySelector("#start").onclick = () => Start()
 
-function Two(){
+document.onkeydown = (event) => { if(event.key === "Enter") Start() }
+
+function Start(){
 	const size = 70;
-	document.body.innerHTML = `<span id="version-font">Version:</span><span id="version-id"> 1.3.3.312</span><br />`;
-	document.body.innerHTML += `<canvas id="c1" width="${size * 10}" height="${size * 10}"></canvas>`;
-	document.body.innerHTML += `<div id="buttoms"></div>`;
-	document.querySelector("#c1").style.backgroundColor = bgcolor;
-	const canvas = document.querySelector('#c1');
-	const buttoms = document.querySelector("#buttoms");
-	buttoms.innerHTML += `<div class="arrow-up"></div>`;
-	buttoms.innerHTML += `<div class="arrow-down"></div>`;
-	buttoms.innerHTML += `<div class="arrow-left"></div>`;
-	buttoms.innerHTML += `<div class="arrow-right"></div>`;
-	const bUp = document.querySelector(".arrow-up");
-	const bLeft = document.querySelector(".arrow-left");
-	const bDown = document.querySelector(".arrow-down");
-	const bRight = document.querySelector(".arrow-right");
+	document.body.innerHTML = `<span id="version-font">Version:</span><span id="version-id"> 1.3.3.312</span><br />`
+	document.body.innerHTML += `<canvas id="c1" width="${size * 10}" height="${size * 10}"></canvas>`
+	document.body.innerHTML += `<div id="buttoms"></div>`
+	const buttons = document.querySelector("#buttoms")
+	buttons.innerHTML += `<div class="arrow-up"></div>`
+	buttons.innerHTML += `<div class="arrow-down"></div>`
+	buttons.innerHTML += `<div class="arrow-left"></div>`
+	buttons.innerHTML += `<div class="arrow-right"></div>`
+	const buttonUp = document.querySelector(".arrow-up")
+	const buttonLeft = document.querySelector(".arrow-left")
+	const buttonDown = document.querySelector(".arrow-down")
+	const buttonRight = document.querySelector(".arrow-right")
 
 	if(navUA.match('iPhone') || navUA.match('Android') || navUA.match('iPad') || navUA.match('RIM')) {
-		bUp.innerHTML +=`<div class="arrow-up-top"></div>`;
-		bUp.innerHTML +=`<div class="arrow-up-bottom"></div>`;
-		bDown.innerHTML +=`<div class="arrow-down-top"></div>`;
-		bDown.innerHTML +=`<div class="arrow-down-bottom"></div>`;
-		bLeft.innerHTML +=`<div class="arrow-left-top"></div>`;
-		bLeft.innerHTML +=`<div class="arrow-left-bottom"></div>`;
-		bRight.innerHTML +=`<div class="arrow-right-top"></div>`;
-		bRight.innerHTML +=`<div class="arrow-right-bottom"></div>`;
+		buttonUp.innerHTML +=`<div class="arrow-up-top"></div>`
+		buttonUp.innerHTML +=`<div class="arrow-up-bottom"></div>`
+		buttonDown.innerHTML +=`<div class="arrow-down-top"></div>`
+		buttonDown.innerHTML +=`<div class="arrow-down-bottom"></div>`
+		buttonLeft.innerHTML +=`<div class="arrow-left-top"></div>`
+		buttonLeft.innerHTML +=`<div class="arrow-left-bottom"></div>`
+		buttonRight.innerHTML +=`<div class="arrow-right-top"></div>`
+		buttonRight.innerHTML +=`<div class="arrow-right-bottom"></div>`
 	}
+	document.querySelector("#c1").style.backgroundColor = bgcolor
+	const canvas = document.querySelector('#c1')
 	const ctx = canvas.getContext('2d');
-	let mb;/*Doesnt let you go abroad*/ 
+
 	let cens;//NO left <- -> right NO up <- -> bottom NO
 	let cdnt = Math.floor(Math.random() * 100);//random
 	let t;//timer
@@ -94,126 +96,136 @@ function Two(){
 	};
 	let pluScore;
 
-	function scoreF(){
-		clearTimeout(t);
-		document.onkeydown = null;
-		pluScore = +((yum - 2) * 10);
-		score += pluScore;
-		localStorage.setItem('score', +score);
-		if(pluScore > bestScore)bestScore = +pluScore;
-		localStorage.setItem('bestscore', bestScore);
-		thend();
-	}//plus to score and best score!//
-
 	function thend(){
 		clearTimeout(t);
-		document.body.innerHTML = `<span id="version-font">Version:</span><span id="version-id"> 1.3.3.312</span>`;
-		document.body.innerHTML += `<div id="center"></div>`;
-		const center = document.querySelector("#center");
-		center.innerHTML = `<img id="gameover" src="Images/GameOver.png" alt=""></img>`;
-		center.innerHTML += `<div class="score" id="scoreITG">Your score in this game: ${pluScore}<div>`;
-		center.innerHTML += `<div class="score" id="bScore">Best score: ${bestScore}<divn>`;
-		center.innerHTML += `<div class="score" id="tScorEv">Total score ever: ${score}<div>`;
-		center.innerHTML += `<img id="restart" src="Images/Restart.png" alt=""></img>`;
-		center.style.background = "black";
-		center.style.height = 600+"px";
+		document.body.innerHTML = `<span id="version-font">Version:</span><span id="version-id"> 1.3.3.312</span>`
+		document.body.innerHTML += `<div id="center"></div>`
+		const center = document.querySelector("#center")
+		center.innerHTML = `<img id="gameover" src="Images/GameOver.png" alt=""></img>`
+		center.innerHTML += `<div class="score" id="scoreITG">Your score in this game: ${pluScore}<div>`
+		center.innerHTML += `<div class="score" id="bScore">Best score: ${bestScore}<divn>`
+		center.innerHTML += `<div class="score" id="tScorEv">Total score ever: ${score}<div>`
+		center.innerHTML += `<img id="restart" src="Images/Restart.png" alt=""></img>`
+		center.style.background = "black"
+		center.style.height = 600 +"px"
 		const restart = document.querySelector("#restart");
 		restart.onmousemove = function() { this.src = "Images/Restart2.png" }
 		restart.onmouseleave = function() { this.src = "Images/Restart.png" }
-		restart.onclick =()=> { location.reload() }
+		restart.onclick = () => location.reload()
 	}//GameOver//
 
-	function stop() {
-		if (r[0] === -size) scoreF();
-		else if (r[0] === size*10) scoreF();
-		else if (u[0] === -size) scoreF();
-		else if (u[0] === size*10) scoreF();
-		else mb = 1;
-		for (i = 5; i < yum; i++) if(r[i] === r[0] && u[i] === u[0]) scoreF();
+	function gameOver(){
+		clearTimeout(t)
+		document.onkeydown = null
+		pluScore = ((+yum - 2) * 10)
+		score += pluScore
+		localStorage.setItem('score', +score)
+		if(pluScore > bestScore) bestScore = +pluScore
+		localStorage.setItem('bestscore', bestScore)
+		thend();
+	}//plus to score and best score!//
+
+	function checkGameOver() {
+		if (r[0] === -size || r[0] === size*10 || u[0] === -size || u[0] === size*10) gameOver()
+
+		for (i = 4; i < yum; i++) if(r[i] === r[0] && u[i] === u[0]) gameOver()
 	}//Game over//
 
-	function move(){if(mb === 1) for(i = 100;i > 0; i--) r[i]=r[i-1], u[i]=u[i-1];}//Snake moves
+	const plusTail = () => {
+		for(i = 100; i > 0; i--) {
+			r[i] = r[i-1]
+			u[i] = u[i-1]
+		};
+	}//Snake moves
 
-	function square(color,x, y) {
-		ctx.beginPath();
-		ctx.fillStyle = color;
-		ctx.moveTo(x, y);
-		ctx.lineTo(size + x, y);
-		ctx.lineTo(size + x, y + size);
-		ctx.lineTo(x, y + size);
-		ctx.lineTo(x, y);
-		ctx.stroke();
-		ctx.fill();
+	function creatingSquare(color, x, y) {
+		ctx.beginPath()
+		ctx.fillStyle = color
+		ctx.moveTo(x, y)
+		ctx.lineTo(size + x, y)
+		ctx.lineTo(size + x, y + size)
+		ctx.lineTo(x, y + size)
+		ctx.lineTo(x, y)
+		ctx.stroke()
+		ctx.fill()
 	}
 
-	function blue(){square(snacolor,r[0], u[0])}//blue square//
+	function minusEat() {
+		let sSX = size * Saves.XSearch, sSY = size * Saves.YSearch
 
-	function minus() {
-		let sSX = size * Saves.XSearch, sSY = size * Saves.YSearch;
 		if (r[0] === sSX && u[0] === sSY) {
-			yum++;
-			Saves.sec -= 4;
-			neW();
+			yum++
+			Saves.sec -= 4
+			newCrdntEat()
 		}
 	}//orng is dying
 
 	function finish() {
 		if (yum === 100) {
-			document.body.style.background = "yellow";
-			alert('Congratulations you won!');
-			location.reload();
+			document.body.style.background = "yellow"
+			alert('Congratulations you won!')
+
+			location.reload()
 		}
 	}//The End(?).
 
-	function neW() {
-		cdnt = Math.floor(Math.random() * 100);//random
+	function newCrdntEat() {
+		cdnt = Math.floor(Math.random() * 100)//random
 
-		Saves.YSearch = Math.floor(cdnt / 10);
-		Saves.XSearch = cdnt % 10;
+		Saves.YSearch = Math.floor(cdnt / 10)
+		Saves.XSearch = cdnt % 10
+		let sSX = size * Saves.XSearch, sSY = size * Saves.YSearch
 
-		let sSX = size * Saves.XSearch, sSY = size * Saves.YSearch;
-		for(i = 0; i < yum; i++)if(r[i] === sSX && u[i] === sSY) neW();
+		for(i = 0; i < yum; i++) if(r[i] === sSX && u[i] === sSY) newCrdntEat()
 	}//rundom number
 
-	function fiX() {square(eacolor, size * Saves.XSearch, size * Saves.YSearch)}//Spawn orng square//
+	const fixEat = () => creatingSquare(eacolor, size * Saves.XSearch, size * Saves.YSearch)//Spawn orng square//
 
-	function plus() {for(i = 0; i < yum; i++)square(snacolor, r[i], u[i])}//+tail
+	const MakingSnake = () => { 
+		for(i = 0; i < yum; i++) creatingSquare(snacolor, r[i], u[i]) 
+	}
 
 	function sequence(){
-		ctx.clearRect(0, 0, size*10, size*10);
-		stop();
-		move();
-		blue();
-		minus();
-		finish();
-		fiX();
-		plus();
+		ctx.clearRect(0, 0, size*10, size*10)
+		checkGameOver()
+		plusTail()
+		minusEat()
+		finish()
+		fixEat()
+		MakingSnake()
 	}//sequence
 
-	const makeTurn = (uORx, one, censNum) => {
-		clearTimeout(t);
-		uORx[0] += one*size;
-		sequence();
-		cens = censNum;		
-		t = setTimeout(makeTurn, Saves.sec, uORx, one, censNum);
+	const makeTurn = (uORx, size, censNum) => {
+		clearTimeout(t)
+		uORx[0] += size
+		sequence()
+		cens = censNum
+		t = setTimeout(makeTurn, Saves.sec, uORx, size, censNum)
 	}//moving
-
-	blue()
 
 	document.onkeydown = (event) => {
 		const ek = event.key
-		const checkEk = (directionKeys, ek) => {return directionKeys.includes(ek);}
+		const checkEk = (directionKeys, ek) => {return directionKeys.includes(ek)}
 
-		if (cens !== 1 && checkEk(['ArrowLeft', 'A', 'a', 'ф', 'Ф'], ek)) makeTurn(r, -1, 3);
-		if (cens !== 2 && checkEk(['ArrowUp', 'W', 'w', 'ц','Ц'], ek)) makeTurn(u, -1, 4);
-		if (cens !== 3 && checkEk(['ArrowRight', 'D', 'd', 'в', 'В'], ek)) makeTurn(r, 1, 1);
-		if (cens !== 4 && checkEk(['ArrowDown', 'S', 's', 'ы', 'Ы'], ek)) makeTurn(u, 1, 2);
+		if (cens !== 1 && checkEk(['ArrowLeft', 'A', 'a', 'ф', 'Ф'], ek)) makeTurn(r, -size, 3)
+		if (cens !== 2 && checkEk(['ArrowUp', 'W', 'w', 'ц','Ц'], ek)) makeTurn(u, -size, 4)
+		if (cens !== 3 && checkEk(['ArrowRight', 'D', 'd', 'в', 'В'], ek)) makeTurn(r, size, 1)
+		if (cens !== 4 && checkEk(['ArrowDown', 'S', 's', 'ы', 'Ы'], ek)) makeTurn(u, size, 2)
 	}
-	bLeft.onclick = () => {if(cens !== 1) makeTurn(r, -1, 3)}//click on left
-	bUp.onclick = () => {if(cens !== 2) makeTurn(u, -1, 4)}//click on up
-	bRight.onclick = () => {if(cens !== 3) makeTurn(r, 1, 1)}//click on right
-	bDown.onclick = () => {if(cens !== 4) makeTurn(u, 1, 2)}//click on down
+	buttonLeft.onclick = () => {
+		if(cens !== 1) makeTurn(r, -size, 3)
+	}//click on left
+	buttonUp.onclick = () => {
+		if(cens !== 2) makeTurn(u, -size, 4)
+	}//click on up
+	buttonRight.onclick = () => {
+		if(cens !== 3) makeTurn(r, size, 1)
+	}//click on right
+	buttonDown.onclick = () => {
+		if(cens !== 4) makeTurn(u, size, 2)
+	}//click on down
 
-	neW();
-	fiX();
+	creatingSquare(snacolor, r[0], u[0])//snakeHead
+	newCrdntEat()
+	fixEat()
 }//Snake
